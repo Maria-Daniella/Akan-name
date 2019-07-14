eca5639bd0507e80f4f0e7d0689d89236255cf0c
maria Daniella <mariadanigikonyo@gmail.com>
2019-07-14 21:37:32 +0300

    add functions to script.js file

---
+++ script.js
@@ -0,0 +1,8 @@
+var century, year, month, dayOfMonth, dayOfWeek, day;
+
+function getUserInput (){
+  century = parseInt(document.getElemetById("century").value);
+  year = parseInt(document.getElemetById("year").value);
+  month = parseInt(document.getElemetById("month").value);
+  dayOfMonth = parseInt(document.getElemetById("day-of-month").value);
+}
